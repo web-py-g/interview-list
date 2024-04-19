ee653de53114a4f703c60f89bee8993319da19e7
web-py-g <nizametdinov22@gmail.com>
2024-04-19 10:59:00 +0300

    Init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { useUserStore } from '@/stores/user';
+import type { ComputedRef } from 'vue';
+
+interface IMenuItem {
+  label: string,
+  icon: string,
+  path: string,
+  show: ComputedRef<boolean>
+}
+
+const userStore = useUserStore()
+
+const items = ref<IMenuItem[]>([
+  {
+    label: 'Авторизация',
+    icon: 'pi pi-user',
+    path: '/auth',
+    show: computed((): boolean => !userStore.userId)
+  },
+  {
+    label: 'Добавить',
+    icon: 'pi pi-plus',
+    path: '/',
+    show: computed((): boolean => !!userStore.userId)
+  },
+  {
+    label: 'Список собеседований',
+    icon: 'pi pi-list',
+    path: '/list',
+    show: computed((): boolean => !!userStore.userId)
+  },
+  {
+    label: 'Статистика',
+    icon: 'pi pi-chart-pie',
+    path: '/statistic',
+    show: computed((): boolean => !!userStore.userId)
+  }
+])
+</script>
+<template>
+  <app-menubar :model="items" class="menu">
+    <template #item="{ item, props }">
+      <template v-if="item.show">
+        <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
+          <span :class="item.icon" class="p-menuitem-icon"></span>
+          <span class="ml-2"> {{ item.label }}</span>
+        </router-link>
+      </template>
+    </template>
+    <template #end>
+      <span v-if="!!userStore.userId" @click="userStore.userId = ''" class="flex align-items-center menu-exit">
+        <span class="pi pi-sign-out p-menuitem-icon"></span>
+        <span class="ml-2">Выход</span>
+      </span>
+    </template>
+  </app-menubar>
+</template>
+<style scoped>
+.menu {
+  margin: 30px 0;
+}
+
+.menu-exit {
+  cursor: pointer;
+}
+</style>
